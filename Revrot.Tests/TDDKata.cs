b2c30772e5f004f7f4c3365f27a22d9e03c70d32
Vladimir Loginov <jiogunoff@gmail.com>
2021-07-07 17:47:20 +0300

    green test

--- Revrot.Tests/TDDKata.cs
+++ Revrot.Tests/TDDKata.cs
@@ -18,6 +18,11 @@ namespace Revrot.Tests
                 return string.Empty;
             }
 
+            if (sz == 1)
+            {
+                return str;
+            }
+            
             if (str.Length == sz && str.Select(x => Math.Pow(int.Parse(x.ToString()), 3)).Sum() % 2 == 0)
             {
                 return new string(str.Reverse().ToArray());
@@ -33,7 +38,7 @@ namespace Revrot.Tests
             var chunksCount = str.Length / sz;
             for (int chunkIdx = 0; chunkIdx < chunksCount; chunkIdx++)
             {
-                var chunk = str.Substring(chunkIdx + sz * chunkIdx, sz);
+                var chunk = str.Substring(sz * chunkIdx, sz);
                 if (chunk.Select(x => Math.Pow(int.Parse(x.ToString()), 3)).Sum() % 2 == 0)
                 {
                     sb.Append(new string(chunk.Reverse().ToArray()));
